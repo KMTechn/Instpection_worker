a86bbadbc3e0c8174e5ed03deeeedb27abe0b9c0
KMTechn <replay121678@naver.com>
2025-08-19 19:07:26 +0900

    Inspection Program의 버전을 v2.0.5로 변경하여 최신 버전 정보를 반영했습니다.

--- Inspection_worker.py
+++ Inspection_worker.py
@@ -36,7 +36,7 @@ except ImportError:
 
 REPO_OWNER = "KMTechn"
 REPO_NAME = "Instpection_worker"
-CURRENT_VERSION = "v2.3.4"
+CURRENT_VERSION = "v2.0.5"
 
 def check_for_updates(app_instance):
     """GitHub에서 최신 릴리스를 확인합니다."""
